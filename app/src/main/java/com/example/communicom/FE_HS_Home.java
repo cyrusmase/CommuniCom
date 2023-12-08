6dc758d302b26dff7d429fef54233590cb154088
cyrusmase <cyrusmase@gmail.com>
2023-12-07 21:55:11 -0800

    committing

--- app/src/main/java/com/example/communicom/FE_HS_Home.java
+++ app/src/main/java/com/example/communicom/FE_HS_Home.java
@@ -2,19 +2,28 @@ package com.example.communicom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FE_HS_Home extends AppCompatActivity
 {
+    private static final String FILE_NAME = "form_submitNews";
     String versionN_H, versionF_H, versionR_H, versionM_H;
+    private List<String[]> elements;
+    public int currentIndex = 0;
+
+    TextView pageContentAView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -22,9 +31,12 @@ public class FE_HS_Home extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fe_hs_home);
 
+        pageContentAView = findViewById(R.id.pageContent_HomeA);
         CharSequence text = "Sending message...";
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(this /* MyActivity */, text, duration);
+
+        elements = readFromFile(this, FILE_NAME);
     }
     public void onNewsClickH(View view){
         Intent intentN = new Intent(this, FE_HS_News.class);
@@ -57,6 +69,67 @@ public class FE_HS_Home extends AppCompatActivity
         startActivity(intentM);
 
         finish();
+    }
+    public void onNextClickH(View view){
+        currentIndex++;
+        if (currentIndex < elements.size()) {
+            // Retrieve the new currentElement based on the updated index
+            String[] currentElement = elements.get(currentIndex);
+
+            // Make sure the array has enough elements to avoid ArrayIndexOutOfBoundsException
+            if (currentElement.length >= 5) {
+                // Update TextViews with information from the new currentElement array
+                pageContentAView.setText("Log Num: " + currentElement[0]);}
+        } else {currentIndex = 0;}
+
+
+    }
+
+    public void onPrevClickH(View view){
+        if (currentIndex > 0) {
+            currentIndex--;
+            String[] currentElement = elements.get(currentIndex);
+
+            if (currentIndex < elements.size()) {
+                // Retrieve the new currentElement based on the updated index
+
+                // Make sure the array has enough elements to avoid ArrayIndexOutOfBoundsException
+                if (currentElement.length >= 1) {
+                    // Update TextViews with information from the new currentElement array
+                    pageContentAView.setText("Log Num: " + currentElement[0]);
+                }
+            } else {
+                currentIndex = 0;
+            }
+        }
+    }
+
+    public static List<String[]> readFromFile(Context context, String fileName) {
+        List<String[]> elements = new ArrayList<>();
+        try {
+            // Open the file for reading
+            FileInputStream fileInputStream = context.openFileInput(fileName);
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            // Read each line from the file
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                // Split the line into an array of strings
+                String[] lineArray = line.split(",");
+                // Add each line (element) to the list
+                elements.add(lineArray);
+            }
+
+            // Close the streams
+            bufferedReader.close();
+            inputStreamReader.close();
+            fileInputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return elements;
     }
     public void onSubmitNewsClickH(View view){
         Intent intentSubmit = new Intent (this, FE_Form_SubmitNews.class);
